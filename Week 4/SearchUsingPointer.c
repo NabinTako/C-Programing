d54ac83f5639728d44da2c6ac9f41e27b8fd5e51
NabinTako <nabintako999@gmail.com>
2022-12-08 11:17:54 +0545

    week 4

--- Week 4/SearchUsingPointer.c
+++ Week 4/SearchUsingPointer.c
@@ -1,7 +1,7 @@
 //Write a C program to search an element in array using pointers.
 #include <stdio.h>
 void main(){
-	int i,sum=0,num;
+	int i,sum=0,num,check;
 	int arr[5]={1,2,3,5,9};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	int *ptr=arr;
@@ -9,7 +9,14 @@ void main(){
 	for(i=0;i<size;i++){
 		if(*(ptr+i) == num){
 			printf("number %d is in index %d in array",num,i);
+			check=0;
+			break;
+		}else{
+			check=1;
 		}
+	}
+	if(check ==1){
+		printf("number %d is not in the array",num);
 	}
 	
 }
