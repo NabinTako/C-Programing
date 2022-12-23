7758cc2053d08bbabcfdbd21685b67095ba0fb73
NabinTako <nabintako999@gmail.com>
2022-12-23 09:16:44 +0545

    Week %

---
+++ Week 5/LRQ5.c
@@ -0,0 +1,4 @@
+/* Write a structure to store the names, salary and hours of work per day of 5
+employees in a company. Write a program to increase the salary depending on the
+number of hours of work per day as follows and then print the name of all the
+employees along with their final salaries. */
