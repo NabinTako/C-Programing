3f3868c040a9fc2cad8e00aefb149ffe5b0e6baa
NabinTako <nabintako999@gmail.com>
2023-01-05 21:48:21 +0545

    Week 8

---
+++ Week 8/asd.c
@@ -0,0 +1,35 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+//    int n,i,sum=0;
+//    scanf("%d",&n);
+//    int *arr = calloc(n,sizeof(int));
+//    for( i=0;i<n;i++){
+//        scanf("%d",(arr+i));
+//        sum = sum + *(arr+i);
+//    }
+//    printf("%d",sum);
+//    free(arr);
+//    
+//}
+// Online C compiler to run C program online
+//#include <stdio.h>
+//
+//int main() {
+//    int arr[4];
+//    int i,j,skip=0;
+//     for( i=0;i<4;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//    for( i=0;i<4;i++){
+//        for(j=0;j<=i;j++){
+//            if(arr[i]==arr[j]){
+//                break;
+//            }
+//        }
+//        if(i==j){
+//            printf("%d ",arr[i]);
+//        }
+//    }
+//    return 0;
+//}
---
+++ Week 8/asds.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int c;
+	int sum=0;
+	int i;
+	printf("enter the size of array");
+	scanf("%d",&c);
+
+	
+int *arr=calloc(c,sizeof(int));
+for(i=0;i<c;i++){
+	sum=sum+arr[i];
+}
+printf("the sum is %d",sum);
+}
+
+
+
