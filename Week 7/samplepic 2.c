05ef239388773a61fd0f23eeb651b4f5c7d476cf
NabinTako <nabintako999@gmail.com>
2023-01-09 23:21:47 +0545

    Week 7

---
+++ Week 7/samplepic 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lodepng.h"
+
+int main()
+{
+    unsigned char *img;
+    unsigned int width, height;
+    unsigned int error;
+
+    error = lodepng_decode32_file(&img, &width, &height, "naruto.png");
+    if (error)
+    {
+        printf("%u : %s\n", error, lodepng_error_text(error));
+    }
+    // Workshop Question 1
+
+    for (int i = 0; i < width * height * 4; i = i + 4)
+    {
+        img[i] = 255 - img[i];
+        img[i + 1] = 255 - img[i + 1];
+        img[i + 2] = 255 - img[i + 2];
+        img[i+3] = 150;
+    }
+    lodepng_encode32_file("-VE Naruto.png", img, width, height);
+    free(img);
+}
